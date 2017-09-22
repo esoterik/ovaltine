eafdd7d31bf505183c7b8952ee1cffe944bfc05a
Sydney Young <sydney.young@yale.edu>
2017-09-22 15:37:47 -0400

    Add RSpec configuration

---
+++ spec/spec_helper.rb
@@ -0,0 +1,21 @@
+%w(ciphers solvers lib).each do |d| 
+  $LOAD_PATH << File.join(File.dirname(__FILE__), '..', d)
+end
+
+$LOAD_PATH << File.join(File.dirname(__FILE__))
+
+require 'byebug'
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.syntax = :expect
+    mocks.verify_partial_doubles = true
+  end
+
+  config.example_status_persistence_file_path = 'tmp/rspec_examples.txt'
+  config.order = :random
+end
